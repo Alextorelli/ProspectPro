d0824e30fbaab3371d5c15459a3a4806add2167d
Alextorelli28@gmail.com <Alextorelli28@gmail.com>
2025-10-10 09:48:53 +0000

    jh

--- supabase/functions/_shared/edge-auth.ts
+++ supabase/functions/_shared/edge-auth.ts
@@ -119,7 +119,7 @@ export async function authenticateRequest(
 export const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type",
+    "authorization, x-client-info, apikey, content-type, x-prospect-session",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
 };
 
